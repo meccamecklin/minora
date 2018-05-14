61775cedd3fc1a4e952e74818135483745bcd424
mecca <mecca.mecklin@gmail.com>
2018-05-14 16:09:49 +0300

    Initial code commit

---
+++ service-flow/src/main/java/com/minora/main/MinoraApplication.java
@@ -0,0 +1,10 @@
+package com.minora.main;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+ 
+@ProcessApplication("Loan Approval App")
+public class MinoraApplication extends ServletProcessApplication {
+  // empty implementation
+}
